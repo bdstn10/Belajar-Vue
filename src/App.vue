6d197afead9618dc924d68eb81460a1c4143781e
bdstn10 <budistn108@gmail.com>
2024-02-11 12:30:04 +0700

    Options API usage on Vue component's logic

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
-<!-- Dibawah merupakan contoh penggunaan Composition API pada logika component Vue -->
-<script setup>
-import { ref,onMounted } from 'vue';
+<!-- Dibawah merupakan contoh penggunaan Options API pada logika component Vue -->
+<script>
+export default {
+  // Properties returned from data() become reactive state 
+  // and will be exposed on 'this'.
+  data() {
+    return {
+      count: 0
+    }
+  },
 
-// reactive state
-const count = ref(0)
+  // Methods are functions that mutate state and trigger updates.
+  // They can be bound as event handler in template
+  methods: {
+    increment() {
+      this.count++
+    }
+  },
 
-// function that mutate state and trigger update
-function increment() {
-  count.value++
+  // Lifecycle hooks are called at different stages 
+  // of a component's lifecycle.
+  // This function will be called when the component is mounted
+  mounted() {
+    console.log(`The initial value of count state is ${this.count}`)
+  }
 }
-
-// lifecycle hooks
-onMounted(() => {
-  console.log(`The initial value of 'count' state is: ${count.value}`)
-})
 </script>
 
 <template>
